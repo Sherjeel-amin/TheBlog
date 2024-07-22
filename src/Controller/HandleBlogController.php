0b1ba774daab195bf3445fbdf58e6ce329b986a4
Sherjeel-amin <sherjeel.mohammad@aspiresys.com>
2024-07-22 18:55:05 +0530

    removed the methods not used from routes, natively html uses GET and POST for form submission and rendering

--- src/Controller/HandleBlogController.php
+++ src/Controller/HandleBlogController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 use App\Entity\Blogs;
+use App\Form\BlogEditFormType;
+use App\Repository\BlogsRepository;
 use App\Repository\CommentsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,16 +15,56 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class HandleBlogController extends AbstractController
 {
-    // ... other methods
+    // Other methods...
+
+    /**
+     * Edit an existing blog post.
+     *
+     * @param Request $request
+     * @param BlogsRepository $blogRepository
+     * @param int $id
+     * 
+     * @return Response
+     */
+    public function edit(Request $request, BlogsRepository $blogRepository, int $id): Response
+    {
+        // Fetch the blog by ID using the repository
+        $blog = $blogRepository->find($id);
+
+        // Check if blog exists and throw an exception if not found
+        if (!$blog) {
+            throw $this->createNotFoundException('No blog found for id '.$id);
+        }
+
+        // Check if the current user is the owner of the blog post
+        if ($blog->getUser() !== $this->getUser()) {
+            throw new AccessDeniedException('You are not allowed to edit this blog post.');
+        }
+
+        $form = $this->createForm(BlogEditFormType::class, $blog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $blog->getId()]);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'form' => $form->createView(),
+            'blog' => $blog,
+        ]);
+    }
 
     /**
      * Delete an existing blog post.
      *
-     * @param Request $request The current request object.
-     * @param CommentsRepository $commentsRepository The repository for Comment entities.
-     * @param EntityManagerInterface $entityManager The entity manager.
+     * @param Request $request
+     * @param CommentsRepository $commentsRepository
+     * @param EntityManagerInterface $entityManager
      * 
-     * @return Response The response object.
+     * @return Response
      */
     public function delete(Request $request, CommentsRepository $commentsRepository, EntityManagerInterface $entityManager): Response
     {
